9a8eac3c8b2a52ced7df761cb10472466f8437e2
bartek1470 <13921437+bartek1470@users.noreply.github.com>
2024-11-05 21:21:54 +0100

    add logging interceptor for `RestClient`

---
+++ src/main/kotlin/pl/bartek/aidevs/LoggingRestClientInterceptor.kt
@@ -0,0 +1,36 @@
+package pl.bartek.aidevs
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.ClientHttpResponse
+import java.io.IOException
+
+class LoggingRestClientInterceptor : ClientHttpRequestInterceptor {
+    @Throws(IOException::class)
+    override fun intercept(
+        request: HttpRequest,
+        body: ByteArray,
+        execution: ClientHttpRequestExecution,
+    ): ClientHttpResponse {
+        log.debug { "REQUEST: ${request.method} ${request.uri}" }
+        log.debug { "HEADERS:\n${request.headers.toMultilineString()}" }
+        log.debug { "BODY:\n${String(body)}" }
+
+        val response: ClientHttpResponse = execution.execute(request, body)
+
+        log.debug { "RESPONSE FROM ${request.method} ${request.uri} -> ${response.statusCode}" }
+        log.debug { "HEADERS:\n${response.headers.toMultilineString()}" }
+        log.debug { "BODY:\n${String(response.body.readAllBytes())}" }
+
+        return response
+    }
+
+    private fun HttpHeaders.toMultilineString(): String = this.toMap().entries.joinToString("\n")
+
+    companion object {
+        private val log = KotlinLogging.logger { }
+    }
+}
