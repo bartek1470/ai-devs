7c36a70e7c2a6553f76dbf1b56659097c26ad169
bartek1470 <13921437+bartek1470@users.noreply.github.com>
2024-12-03 22:02:20 +0100

    uncomment prompt cache

--- src/main/kotlin/pl/bartek/aidevs/ai/ChatService.kt
+++ src/main/kotlin/pl/bartek/aidevs/ai/ChatService.kt
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.stream.Collectors
+import kotlin.io.path.exists
 
 @Service
 class ChatService(
@@ -33,12 +34,12 @@ class ChatService(
         val cachedName = "${messages.hashCode()}.txt"
 
         val cachedPath = cacheDir.resolve(cachedName)
-//        if (cachedPath.exists()) {
-//            log.debug { "Using cached response $cachedName" }
-//            val cachedResponse = Files.readString(cachedPath)
-//            onPartialResponseReceived.invoke(cachedResponse)
-//            return cachedResponse
-//        }
+        if (cachedPath.exists()) {
+            log.debug { "Using cached response $cachedName" }
+            val cachedResponse = Files.readString(cachedPath)
+            responseReceived.invoke(cachedResponse)
+            return cachedResponse
+        }
 
         log.debug { "Cached file $cachedName doesn't exist. Calling chat client" }
 
