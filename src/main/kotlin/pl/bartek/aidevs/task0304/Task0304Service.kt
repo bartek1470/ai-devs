66bec76df0373c80605be8321fe31b2117e3bd84
bartek1470 <13921437+bartek1470@users.noreply.github.com>
2024-12-05 21:29:26 +0100

    task 03 04

--- src/main/kotlin/pl/bartek/aidevs/task0304/Task0304Service.kt
+++ src/main/kotlin/pl/bartek/aidevs/task0304/Task0304Service.kt
@@ -3,14 +3,15 @@ package pl.bartek.aidevs.task0304
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.jline.terminal.Terminal
-import org.springframework.ai.chat.messages.SystemMessage
 import org.springframework.ai.chat.messages.UserMessage
 import org.springframework.ai.model.function.DefaultFunctionCallbackBuilder
-import org.springframework.ai.model.function.FunctionCallingOptionsBuilder
+import org.springframework.ai.model.function.FunctionCallingOptionsBuilder.PortableFunctionCallingOptions
 import org.springframework.ai.openai.api.OpenAiApi
+import org.springframework.ai.openai.api.OpenAiApi.ChatModel
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ansi.AnsiColor.BRIGHT_YELLOW
 import org.springframework.boot.ansi.AnsiColor.YELLOW
@@ -23,9 +24,10 @@ import pl.bartek.aidevs.course.api.AiDevsAnswer
 import pl.bartek.aidevs.course.api.AiDevsApiClient
 import pl.bartek.aidevs.course.api.Task
 import pl.bartek.aidevs.util.ansiFormatted
-import pl.bartek.aidevs.util.ansiFormattedError
+import pl.bartek.aidevs.util.ansiFormattedAi
+import pl.bartek.aidevs.util.ansiFormattedSecondaryInfo
+import pl.bartek.aidevs.util.ansiFormattedSecondaryInfoTitle
 import pl.bartek.aidevs.util.extractXmlRoot
-import pl.bartek.aidevs.util.print
 import pl.bartek.aidevs.util.println
 import pl.bartek.aidevs.util.replaceNonBreakingSpaces
 import pl.bartek.aidevs.util.stripAccents
@@ -47,141 +49,167 @@ class Task0304Service(
 ) {
     private val cacheDir = cacheDir.resolve(TaskId.TASK_0304.cacheFolderName())
 
+    private val xmlMapper =
+        XmlMapper
+            .builder()
+            .defaultUseWrapper(false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+            .build()
+            .registerKotlinModule()
+
     init {
         Files.createDirectories(this.cacheDir)
     }
 
     fun run(terminal: Terminal) {
-        val note = fetchInputData().replaceNonBreakingSpaces()
-        val apiLog = mutableListOf<AskApiLogEntry>()
-        try {
-            val aiAnswer =
-                chatService.sendToChat(
-                    listOf(
-                        SystemMessage(
-                            """
-                            # Task
-                            
-                            Find in which city Barbara Zawadzka is currently located.
-                            
-                            # Details
-                            
-                            1. User will send you a note about Barbara Zawadzka.
-                            2. The note is from the past, so the data inside is out of date, because you need to find current location.
-                            3. The note's content is in Polish language.
-                            4. The note also mentions other people and different places.
-                            5. Barbara Zawadzka is NOT in Kraków city.
-                            5. Barbara Zawadzka is NOT in Warszawa city.
-                            5. Barbara Zawadzka is NOT in Lublin city.
-                            
-                            # Rules
-                            
-                            Each time you receive an answer from a tool, output it with a asked query in following format:
-                            <tool>
-                                <query>ASKED QUERY</query>
-                                <answer>TOOL ANSWER</answer>
-                            </tool>
-                            
-                            Under no circumstances, you are not allowed to ask the same query again.
-                            The outputted query and answers should help you with further analysis.
-                            
-                            # How to complete the task
-                            
-                            1. Analyze the note and output that cities and people you can identify there.
-                            2. Using provided tools find out who was coworker of Aleksander Ragowski and Barbara Zawadzka and output it.
-                            3. Using provided tools find out who met Rafał Bomba and output it.
-                            4. Investigate your analysis and findings to determine current location of Barbara Zawadzka or decide what next steps you need to take in order to get it and execute those.
-                            6. Output current location of Barbara Zawadzka in Polish language in XML tag `result`
-                            """.trimIndent(),
-                        ),
-                        UserMessage(note),
-                    ),
-                    listOf(
-                        DefaultFunctionCallbackBuilder()
-                            .description(
-                                """
-                                Get cities where a person with provided first name is located.
-                                The response is in Polish language without declension and accents.
-                                The response can contain multiple names separated with a ONE space.
-                                """.trimIndent(),
-                            ).function(
-                                "people",
-                                SendAskApiRequest(apiKey, restClient, apiUrl, "people") {
-                                    apiLog.add(it)
-                                    terminal.println()
-                                    terminal.println("People".ansiFormatted(style = BOLD, color = BRIGHT_YELLOW))
-                                    terminal.println(
-                                        "${"Q:".ansiFormatted(
-                                            color = YELLOW,
-                                        )} ${it.query}\n${"A:".ansiFormatted(color = YELLOW)} ${it.response}",
-                                    )
-                                },
-                            ).inputType(AskApiQuery::class.java)
-                            .build(),
-                        DefaultFunctionCallbackBuilder()
-                            .description(
-                                """
-                                Get people which are located in a provided city.
-                                The response is in Polish language without declension and accents.
-                                The response can contain multiple places separated with a ONE space.
-                                """.trimIndent(),
-                            ).function(
-                                "places",
-                                SendAskApiRequest(apiKey, restClient, apiUrl, "places") {
-                                    apiLog.add(it)
-                                    terminal.println()
-                                    terminal.println("Places".ansiFormatted(style = BOLD, color = BRIGHT_YELLOW))
-                                    terminal.println(
-                                        "${"Q:".ansiFormatted(
-                                            color = YELLOW,
-                                        )} ${it.query}\n${"A:".ansiFormatted(color = YELLOW)} ${it.response}",
-                                    )
-                                },
-                            ).inputType(AskApiQuery::class.java)
-                            .build(),
-                    ),
-                    chatOptions =
-                        FunctionCallingOptionsBuilder.PortableFunctionCallingOptions
-                            .builder()
-                            .withModel(
-//                                OpenAiApi.ChatModel.GPT_4_O_MINI.value,
-                                OpenAiApi.ChatModel.GPT_4_O.value,
-                            ).build(),
-//                streaming = false,
-                ) {
-                    terminal.print(it)
-                }
-            terminal.println()
+        val summaryWithLists = summarizeInputData()
+        val summary = summaryWithLists.substring(0, summaryWithLists.indexOf("<")).trim()
+        val people =
+            summaryWithLists.extractXmlRoot("people")!!.let { xmlMapper.readValue<String>(it) }.trim().split("\n").map { it.trim() }
+        val places =
+            summaryWithLists.extractXmlRoot("places")!!.let { xmlMapper.readValue<String>(it) }.trim().split("\n").map { it.trim() }
 
-            val result =
-                aiAnswer
-                    .extractXmlRoot()
-                    ?.let {
-                        val mapper =
-                            XmlMapper
-                                .builder()
-                                .defaultUseWrapper(false)
-                                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-                                .build()
-                                .registerKotlinModule()
-                        mapper.readValue(it, String::class.java)
-                    }?.stripAccents()
-                    ?.uppercase()
-                    ?: "idk"
+        terminal.println("Note summary:".ansiFormattedSecondaryInfoTitle())
+        terminal.println(summary.ansiFormattedSecondaryInfo())
+        terminal.println("People:".ansiFormattedSecondaryInfoTitle())
+        terminal.println(people.joinToString(", ").ansiFormattedSecondaryInfo())
+        terminal.println("Places:".ansiFormattedSecondaryInfoTitle())
+        terminal.println(places.joinToString(", ").ansiFormattedSecondaryInfo())
+
+        val aiAnswer =
+            chatService.sendToChat(
+                listOf(
+                    UserMessage(
+                        """
+                        |# Summary of note from the past
+                        |$summary
+                        |
+                        |# People
+                        |$people
+                        |
+                        |# Places
+                        |$places
+                        |
+                        |Treat the note summary, list of people and list of places as a starting point to find Barbara Zawadzka current location.
+                        |If provided tools returns BARBARA and the city appeared in the above data then it means it was a past location and you still need to do the searching.
+                        |Ask about every possible person and place from above data and tool responses.
+                        |List the possible locations in `result` XML tag like this:
+                        |<result>
+                        |Kołobrzeg
+                        |Szczecin
+                        |</result>
+                        """.trimMargin(),
+                    ),
+                ),
+                listOf(
+                    DefaultFunctionCallbackBuilder()
+                        .description(
+                            """
+                            Get cities where a person with provided first name is currently located. Accepts only one Polish word without declension and accents. The response can contain multiple names.
+                            """.trimIndent(),
+                        ).function(
+                            "people",
+                            SendAskApiRequest(apiKey, restClient, apiUrl, "people") {
+                                terminal.println()
+                                terminal.println("People".ansiFormatted(style = BOLD, color = BRIGHT_YELLOW))
+                                terminal.println(
+                                    "${
+                                        "Q:".ansiFormatted(
+                                            color = YELLOW,
+                                        )
+                                    } ${it.query}\n${"A:".ansiFormatted(color = YELLOW)} ${it.response}",
+                                )
+                            },
+                        ).inputType(AskApiQuery::class.java)
+                        .build(),
+                    DefaultFunctionCallbackBuilder()
+                        .description(
+                            """
+                            Get people which is currently located in a queried city. Accepts only one Polish word without declension and accents. The response can contain multiple places.
+                            """.trimIndent(),
+                        ).function(
+                            "places",
+                            SendAskApiRequest(apiKey, restClient, apiUrl, "places") {
+                                terminal.println()
+                                terminal.println("Places".ansiFormatted(style = BOLD, color = BRIGHT_YELLOW))
+                                terminal.println(
+                                    "${
+                                        "Q:".ansiFormatted(
+                                            color = YELLOW,
+                                        )
+                                    } ${it.query}\n${"A:".ansiFormatted(color = YELLOW)} ${it.response}",
+                                )
+                            },
+                        ).inputType(AskApiQuery::class.java)
+                        .build(),
+                ),
+                chatOptions = PortableFunctionCallingOptions.builder()
+                    .withTemperature(0.5)
+                    .withModel(ChatModel.GPT_4_O.value)
+                    .build()
+            )
+
+        terminal.println("AI:".ansiFormattedAi())
+        terminal.println(aiAnswer)
+
+        val multipleAnswers =
+            aiAnswer
+                .extractXmlRoot()
+                ?.let {
+                    xmlMapper.readValue(it, String::class.java)
+                }?.stripAccents()
+                ?.uppercase()
+                ?: "idk"
+        val resultList = multipleAnswers.split("\n").filter { it.isNotBlank() }.map { it.trim() }
+
+        resultList.forEach { result ->
             val answer = aiDevsApiClient.sendAnswer(answerUrl, AiDevsAnswer(Task.LOOP, result))
             terminal.println(answer)
-        } catch (ex: Exception) {
-            val errorMessage = "Failed to determine location of Barbara Zawadzka."
-            log.error(ex) { errorMessage }
-            terminal.println()
-            terminal.println(errorMessage.ansiFormattedError())
+        }
+    }
+
+    private fun summarizeInputData(): String {
+        val cachedSummaryPath = cacheDir.resolve("summaryWithLists.txt")
+        if (cachedSummaryPath.exists()) {
+            return Files.readString(cachedSummaryPath)
         }
 
-//        terminal.println("API log:".ansiFormatted(style = BOLD, color = BRIGHT_YELLOW))
-//        terminal.println(
-//            "\t${apiLog.joinToString("\n\t") { "${"Q:".ansiFormatted(color = YELLOW)} ${ it.query }\n\tA: ${ it.response }" }}",
-//        )
+        val note = fetchInputData().replaceNonBreakingSpaces()
+        val summaryWithLists =
+            chatService.sendToChat(
+                listOf(
+                    UserMessage(
+                        """
+                        Note about Barbara Zawadzka:
+                        $note
+                        
+                        Above is a note about Barbara Zawadzka. The note is from the past. Summarize it and in the end list all people and places appearing in that note.
+                        In the summary focus on people and places and omit information that is not relevant to traveling or meeting people.
+                        People list should include only names. Places list should be in Polish language.
+                        Example result:
+                        
+                        Barbara Zawadzka is a woman. She was living in Warsaw. She met Aleksander Kowalski.
+                        <people>
+                        Barbara Zawadzka
+                        Aleksander Kowalski
+                        </people>
+                        <places>
+                        Paryż
+                        </places>
+                        """.trimIndent(),
+                    ),
+                ),
+                chatOptions =
+                PortableFunctionCallingOptions
+                    .builder()
+                    .withTemperature(0.0)
+                        .withModel(ChatModel.GPT_4_O.value)
+                    .build(),
+            )
+
+        Files.writeString(cachedSummaryPath, summaryWithLists)
+        return summaryWithLists
     }
 
     private fun fetchInputData(): String {
